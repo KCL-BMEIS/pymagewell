13c24ddb217baa939221530bc5398c6033e9b609
crnbaker <christian.baker@kcl.ac.uk>
2022-04-01 16:18:32 +0100

    Added test pypi publishing workflow

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,8 +1,8 @@
 echo 'Generating documentation with pdoc...'
 pdoc -t . --docformat google -o ./docs ./pymagewell
-sed -i s/index.html/contents.html/g' docs/pymagewell.html
+sed -i 's/index.html/contents.html/g' docs/pymagewell.html
 
 mv docs/index.html docs/contents.html
-sed -i s/pymagewell.html/index.html/g' docs/contents.html
+sed -i 's/pymagewell.html/index.html/g' docs/contents.html
 
 mv docs/pymagewell.html docs/index.html

4f96559a9bba831eb37b8fc4df4dddf2e8482be5
crnbaker <christian.baker@kcl.ac.uk>
2023-05-05 11:57:52 +0100

    #8 - updated CI and linter script to use new src path

--- linters_and_black.bat
+++ linters_and_black.bat
@@ -1,9 +1,9 @@
 echo 'Formatting code with black...'
-black --line-length 120 pymagewell
-black --line-length 120 tests
+black --line-length 120 src/pymagewell
+black --line-length 120 src/tests
 echo 'Running mypy...'
-mypy pymagewell
-mypy tests
+mypy src/pymagewell
+mypy src/tests
 echo 'Running flake8...'
-flake8 pymagewell
-flake8 tests
+flake8 src/pymagewell
+flake8 src/tests

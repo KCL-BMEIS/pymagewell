278d0da71ca104d5fbe31e3fc9d0b19430563316
crnbaker <christian.baker@kcl.ac.uk>
2023-05-09 14:22:39 +0100

    #7 - modified VideoFrame.as_array() to take account of ColourFormat and decode to numpy array accordingly

--- src/pymagewell/video_frame.py
+++ src/pymagewell/video_frame.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Union
 
-from numpy import uint8, frombuffer
+from cv2 import imdecode
+from numpy import floor, uint16, uint8, frombuffer
 from numpy.typing import NDArray
 
 from pymagewell.pro_capture_device.device_settings import (
+    ColourFormat,
     ImageSizeInPixels,
 )
 
@@ -22,11 +25,23 @@ class VideoFrame:
     string_buffer: bytes
     dimensions: ImageSizeInPixels
     timestamps: VideoFrameTimestamps
+    format: ColourFormat
 
-    def as_array(self) -> NDArray[uint8]:
-        # frame = cast(NDArray[uint8], imdecode(frombuffer(self.string_buffer, dtype=uint8), IMREAD_COLOR))
-        # frame = resize(frame, (self.dimensions.rows, self.dimensions.cols))
-        # return frame
-        numpy_image = frombuffer(self.string_buffer, dtype=uint8, count=self.dimensions.cols * self.dimensions.rows * 3)
-        numpy_image.resize((self.dimensions.rows, self.dimensions.cols, 3))
-        return numpy_image
+    def as_array(self) -> NDArray[Union[uint8, uint16]]:
+        # Create array with correct dtype
+        arr: NDArray[Union[uint8, uint16]] = frombuffer(
+            self.string_buffer, dtype=_choose_numpy_dtype_from_colour_format(self.format)
+        )
+        # Fill it with the frame using correct fourcc code
+        frame: NDArray[Union[uint8, uint16]] = imdecode(arr, self.format.value)
+        return frame
+
+
+def _choose_numpy_dtype_from_colour_format(colour_format: ColourFormat) -> type:
+    bits_per_sample_per_channel = int(floor(colour_format.bits_per_pixel / colour_format.num_channels))
+    if bits_per_sample_per_channel <= 8:
+        return uint8
+    elif bits_per_sample_per_channel <= 16:
+        return uint16
+    else:
+        raise ValueError("ColourFormat has unrecognised structure.")
